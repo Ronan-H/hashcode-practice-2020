0dc5a4460f68aa5034de4b7956537d5e64f5e7b0
Mindaugas Sharskus <g00339629@gmit.ie>
2020-02-08 13:53:21 +0000

    first run gready

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,16 +14,34 @@ public class Main {
 
         String fileName = "./input/d_quite_big.in";
 
-        var arr = Files.newBufferedReader(Paths.get(fileName))
+        var arr = getPizzas(fileName);
+//        System.out.println(Arrays.toString(arr));
+
+        var solver = new Greedy(1_000_000_000, arr).getSolution();
+
+        writeToFile("output", solver);
+
+    }
+
+    // read file get pizzas
+    private static int[] getPizzas(String fileName) throws IOException {
+        return Files.newBufferedReader(Paths.get(fileName))
                 .lines()
                 .skip(1)
                 .map(it -> it.split(" "))
                 .flatMap(Arrays::stream)
                 .mapToInt(Integer::valueOf)
                 .toArray();
+    }
 
+    private static void writeToFile(String filename, int[] arr) throws IOException {
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get(filename));
+        writer.write(arr.length +"\n");
 
-        System.out.println(Arrays.toString(arr));
+        for(var s : arr){
+            writer.write(s +" ");
+        }
 
+        writer.close();
     }
 }
